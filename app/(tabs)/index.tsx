8d568dc45a2b85261ba055f12dd7eefe0ac1f1e9
Santiago pabon <santiago.pabon72@gmail.com>
2026-02-02 18:34:48 -0700

    adding tabs

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+import {Header} from "../../components/ui/header";
+
+export default function Index() {
+  return (
+<Header/>
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   <Text>Games</Text>
+    // </View>
+  );
+}
