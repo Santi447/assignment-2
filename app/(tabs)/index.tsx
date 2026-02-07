ec2fba01a1c055ec725dd62492f95acedaf3c307
Santiago pabon <santiago.pabon72@gmail.com>
2026-02-06 17:13:51 -0700

    added needed text to resemble the screenshot

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,40 @@
-import { Text, View,StyleSheet, ScrollView } from "react-native";
 import GameCardBox from "@/components/GameCardBox";
 import games from "@/data/games.json";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <ScrollView>
-      <View style={style.viewStyle} >
-        <View style={{marginBottom: 20}}>
-        {games.map((game) => (
-          <GameCardBox key={game.id} {...game}/>
-        ))}
+      <View style={style.viewStyle}>
+        <Text style={style.allMatchesText}>All Matches</Text>
+        <Text style={style.competitionText}>{`Competition you don't follow`}</Text>
+        <View style={{ marginBottom: 20 }}>
+          {games.map((game) => (
+            <GameCardBox key={game.id} {...game} />
+          ))}
         </View>
       </View>
     </ScrollView>
   );
 }
 
-
 const style = StyleSheet.create({
   viewStyle: {
-        flex: 1,
-
-        backgroundColor: "black",
-  }
-})
+    flex: 1,
+    backgroundColor: "black",
+    paddingTop: 35,
+  },
+  allMatchesText: {
+    color: "white",
+    marginLeft: "5%",
+    paddingTop: 40,
+    position: "absolute",
+    fontWeight: "bold",
+  },
+  competitionText: {
+    color: "grey",
+    marginLeft: "5%",
+    position: "absolute",
+    paddingTop: 55,
+  },
+});
